d0fd87aa9c503bb1710d9400e390ff4977755554
Vasiliy <v.nichnikov@gmail.com>
2022-08-08 11:14:04 +0300

    Edit files in the Editor

--- Assets/Editor/Bubble/SampleSpriteEditor.cs
+++ Assets/Editor/Bubble/SampleSpriteEditor.cs
@@ -2,7 +2,7 @@
 using UnityEditor;
 using UnityEngine;
 
-namespace Editor
+namespace Editor.Bubble
 {
     [CustomEditor(typeof(SampleBubble))]
     public class SampleSpriteEditor : UnityEditor.Editor
